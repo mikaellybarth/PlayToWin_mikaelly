946e6caa5d9640dc4157f16ee666983c865c4f60
mikaellybarth <wwwmikasouza06@gmail.com>
2024-03-23 14:52:41 -0400

    commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const conn = require("./db/conn");
 
-conn.authenticate().then(() => {
+conn
+.authenticate()
+.then(() => {
     console.log("Conectado ao banco de dados com sucesso!");
 }).catch((err) => {
-    console.log("Ocorreu um erro:" + err)
+    console.log("Ocorreu um erro:" + err);
 });
