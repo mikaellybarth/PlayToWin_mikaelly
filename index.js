96377d4bd3864965ce05e69155a35051b4300c14
mikaellybarth <wwwmikasouza06@gmail.com>
2024-05-23 15:10:39 -0400

    commit
    dando certo por enquanto

--- index.js
+++ index.js
@@ -19,11 +19,25 @@ app.use(
         extended: true,
     })
 )
+
+///////paginas
 app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.render("home");
+});
+
+app.get("/usuarios", async (req,res)=>{
+  
+    const usuarios = await Usuario.findAll({ raw: true });
+
+    res.render("usuarios", { usuarios });
+});
 
 app.get("/usuarios/novo",(req,res)=>{
     res.render("formUsuario");
 });
+
 
 app.post("/usuarios/novo", async (req, res) => {
     try {
